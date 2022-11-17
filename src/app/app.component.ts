29a551acd179a73d620e35281328786b55a4dff3
soumya jain <iamsoumya06@gmail.com>
2022-11-17 22:06:18 +0530

    my counterapp

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'counter';
+  countNumber = 0;
+
+  resetbtn() {
+    this.countNumber = 0;
+  }
+
+  increasebtn() {
+    this.countNumber = this.countNumber + 1;
+  }
+
+  decresebtn() {
+    this.countNumber = this.countNumber - 1;
+  }
+}
